73a76863c43d0e06d4762da8d1db4ddb68d68b31
Pedro Muñoz <pmz94@hotmail.com>
2021-09-20 00:08:56 -0700

    Primer commit

--- database/seeds/ProyectoTableSeeder.php
+++ database/seeds/ProyectoTableSeeder.php
@@ -22,6 +22,72 @@ class ProyectoTableSeeder extends Seeder {
 				'raza' => 'Pitbull',
 				'created_at' => new DateTime,
 				'updated_at' => new DateTime
+			],
+			[
+				'nombre' => 'Bugs',
+				'raza' => 'Conejo Blanco',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Rocko',
+				'raza' => 'Beagle',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Chiquita',
+				'raza' => 'Pitbull',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Hulk',
+				'raza' => 'Chihuahua',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Borrego',
+				'raza' => 'Chow Chow',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Fido',
+				'raza' => 'Corriente',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Titan',
+				'raza' => 'Pitbull',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Maya/Gorda',
+				'raza' => 'Beagle',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Yuki',
+				'raza' => 'Corriente',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Thiago',
+				'raza' => 'Chihuahua/French',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
+            ],
+			[
+				'nombre' => 'Mia',
+				'raza' => 'Pug',
+				'created_at' => new DateTime,
+				'updated_at' => new DateTime
 			]
 		);
 		Proyecto::insert($data);
